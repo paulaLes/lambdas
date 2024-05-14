f2b2556c00aca11945ded014e6f24560cd939c81
paulaLes <paula.lesiczka@gmail.com>
2024-05-14 11:53:03 +0200

    Update threeWays.cpp

--- exercises/threeWays.cpp
+++ exercises/threeWays.cpp
@@ -8,8 +8,17 @@
 // * functor
 // * function
 
+struct divisibleBy3 {
+    divisibleBy3(int n) : n_{n} {}
+    bool operator()(int number) {
+    return number % n_ == 0;
+}
+private: 
+int n_;
+};
+
 int main() {
     std::vector numbers = {18, 21, 36, 90, 27, 14, 103};
-
-    return 0;
+    bool result = std::all_of(numbers.begin(), numbers.end(), divisibleBy3{3});
+    return result;
 }
