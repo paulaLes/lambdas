90838ffa8366e2191b06fb6be5aa11d27575eed1
paulaLes <paula.lesiczka@gmail.com>
2024-06-04 09:49:56 +0000

    func template

---
+++ homework/schedule/schedule.hpp
@@ -0,0 +1,10 @@
+#include <chrono>
+#include <iostream>
+#include <functional> 
+#include <thread>
+
+template <typename Func, typename... Args>
+void schedule(Func func, std::chrono::seconds duration, Args... args) {
+  std::this_thread::sleep_for(duration);
+  func(args...);
+}
